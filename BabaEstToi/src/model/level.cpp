ed36ef6db108c008fb6c4a46f52df617c491e712
58089 <58089@etu.he2b.be>
2023-03-08 10:16:29 +0100

    Début lecture string du fichier

--- BabaEstToi/src/model/level.cpp
+++ BabaEstToi/src/model/level.cpp
@@ -5,12 +5,22 @@
 
 Level::Level(std::string lvl)
 {
+    std::istringstream str {lvl};
 
-    std::stringstream str {lvl};
-
+    /* getting the dimensions */
     std::string tmp;
     std::getline(str, tmp);
 
-    width = std::stoi(tmp);
+    size_t pos = 0;
+    width = std::stoi(tmp, &pos);
+    height = std::stoi(tmp, &pos + 1);
+
+    while(std::getline(str, tmp))
+    {
+        std::istringstream line {tmp};
+        std::string tmp2;
+        std::getline(line, tmp2, ' ');
+        // TODO : tile = first, etc.
+    }
     // TODO : strtok pour scanner les differents mots
 }
